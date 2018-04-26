88e92d067800a94f4cf4747263fd8a9d421ed46b
kangjianlong <kangjianlong@tulibj.com>
2018-04-26 15:12:32 +0800

    提交

--- src/containers/usercontent/mapcomponent/titlewindows/Titlewindows.js
+++ src/containers/usercontent/mapcomponent/titlewindows/Titlewindows.js
@@ -19,16 +19,16 @@ class Titlewindows extends Component {
             <div className="titlewindow" style={gridStyle} id="titlewindowid">
                 <Card title="属性信息" extra={[ <Button type="primary" icon="close" onClick={titleWindowClose}  />]} style={{ width: 300,header:25 }}>
                     <div>
-                            {/*<div className="dropdown">*/}
-                                {/*<button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="false" aria-expanded="true">*/}
-                                    {/*条件*/}
-                                {/*</button>*/}
-                                {/*<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">*/}
-                                    {/*<a className="dropdown-item" >高铁</a>*/}
-                                    {/*<a className="dropdown-item" >火车</a>*/}
-                                    {/*<a className="dropdown-item" >动车</a>*/}
-                                {/*</div>*/}
-                            {/*</div>*/}
+                        {/*<div className="dropdown">*/}
+                        {/*<button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="false" aria-expanded="true">*/}
+                        {/*条件*/}
+                        {/*</button>*/}
+                        {/*<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">*/}
+                        {/*<a className="dropdown-item" >高铁</a>*/}
+                        {/*<a className="dropdown-item" >火车</a>*/}
+                        {/*<a className="dropdown-item" >动车</a>*/}
+                        {/*</div>*/}
+                        {/*</div>*/}
                         <Search
                             placeholder="请输入查询条件"
                             onSearch={value => console.log(value)}
@@ -44,7 +44,7 @@ class Titlewindows extends Component {
                             <Input placeholder="任务时间"/>
                             <Input placeholder="任务属于"/>
                         </Form>
-                    <br/>
+                        <br/>
                         <Button onClick={this.saveClick}>提交</Button>
                         &nbsp;
                         {/*<a href="#" class="btn btn-primary">下一步</a>*/}
@@ -56,10 +56,10 @@ class Titlewindows extends Component {
 
 }
 export function titleWindowClose(state) {
-        if(state=="true"){
-            document.getElementById("titlewindowid").style.display="";
-        }else{
-            document.getElementById("titlewindowid").style.display="none";
-        }
+    if(state=="true"){
+        document.getElementById("titlewindowid").style.display="";
+    }else{
+        document.getElementById("titlewindowid").style.display="none";
+    }
 }
 export default Titlewindows;
